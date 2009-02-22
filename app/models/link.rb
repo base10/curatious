33dc927d2c0f413c895a64c48eb44170d087c253
Nathan L. Walls <nathan@rexluther.com>
2009-02-22 00:02:06 -0500

    Added spec tests to link_spec and adjusted link model to pass them.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,11 @@
 class Link < ActiveRecord::Base
   belongs_to :topic
+
+  validates_length_of :title,       :in      => 3..255
+  validates_length_of :slug,        :in      => 3..255
+  validates_length_of :url,         :in      => 12..255
+  validates_length_of :description, :minimum => 5
+
+  ## TODO: Verify the format of the URL
+
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -4,6 +4,10 @@ describe Link do
   fixtures :topics, :links
 
   before(:each) do
+    @link = links(:foo_link)
+  end
+
+  it "should create a new instance given valid attributes" do
     @valid_attributes = {
       :slug        => "value for slug",
       :title       => "value for title",
@@ -11,11 +15,44 @@ describe Link do
       :url         => "value for url",
       :topic       => topics(:bar)
     }
+
+    Link.create!(@valid_attributes)
   end
 
+  %w(slug title description url).each do |method|
+    it "should have a #{method} " do
+      @link.send( method + '=', nil )
+      @link.should_not be_valid
+    end
+  end
 
-  it "should create a new instance given valid attributes" do
-    Link.create!(@valid_attributes)
-    
+  %w(slug title).each do |method|
+    it "should require #{method} to have at least three characters" do
+      @link.send( method + '=', get_string(2) )
+      @link.should_not be_valid
+    end
+
+    it "should require #{method} to have no more than 255 characters" do
+      @link.send( method + '=', get_string(256) )
+      @link.should_not be_valid
+    end
+  end
+
+  it "should require slug to be unique for a given topic" do
+    pending
+  end
+
+  it "should require url to have at least 12 characters" do
+    @link.url = 'http://a.co';
+    @link.should_not be_valid
+  end
+
+  it "should require url to have no more than 255 characters" do
+    @link.url = 'http://' + get_string( 246 ) + '.us';
+    @link.should_not be_valid
+  end
+
+  it "should require url's format to be valid" do
+    pending
   end
 end
