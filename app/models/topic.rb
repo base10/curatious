6e3769ad3208dacc4fbed10b1d8d5c41133bd831
Nathan L. Walls <nathan@rexluther.com>
2009-02-21 23:25:38 -0500

    Added validation tests to topic spec and added necessary validations for the tests to pass.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,8 @@
 class Topic < ActiveRecord::Base
   has_many :links
+  
+  validates_length_of :name, :in => 3..255
+  validates_length_of :slug, :in => 3..255
+
+  validates_length_of :description, :minimum => 5
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,8 +3,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Topic do
   fixtures :topics
 
-#   before(:each) do
-#   end
+  before(:each) do
+    @topic = topics(:foo)
+  end
 
   it "should create a new instance given valid attributes" do
     @valid_attributes = {
@@ -14,5 +15,24 @@ describe Topic do
     }
 
     Topic.create!(@valid_attributes)
+  end
+
+  %w(slug name description).each do |method|
+    it "should have a #{method} " do
+      @topic.send( method + '=', nil )
+      @topic.should_not be_valid
+    end
+  end
+
+  %w(slug name).each do |method|
+    it "should require #{method} to have at least three characters" do
+      @topic.send( method + '=', get_string(2) )
+      @topic.should_not be_valid
+    end
+
+    it "should require #{method} to have no more than 255 characters" do
+      @topic.send( method + '=', get_string(256) )
+      @topic.should_not be_valid
+    end
   end
 end
